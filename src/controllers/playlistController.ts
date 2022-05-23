ef44e8b4d64d7db5339702f27ef1f1e075a47de7
rafaelarissa <rafaela.larissalj@gmail.com>
2022-05-23 15:51:22 -0300

    feat: added validation in controller

--- src/controllers/playlistController.ts
+++ src/controllers/playlistController.ts
@@ -1,7 +1,14 @@
 import { Request, Response } from "express";
 import { playlistService } from "../services/playlistService.js";
+import { playlistSchema } from "../schemas/playlistSchema.js";
+import { wrongSchemaError } from "../utils/errorUtils.js";
 
 async function insert(req: Request, res: Response) {
+  const validation = playlistSchema.validate(req.body);
+  if (validation.error) {
+    throw wrongSchemaError();
+  }
+
   await playlistService.insert(req.body);
 
   res.sendStatus(201);
--- src/schemas/playlistSchema.ts
+++ src/schemas/playlistSchema.ts
@@ -1,5 +1,6 @@
 import joi from "joi";
 import { CreatePlaylistData } from "../services/playlistService.js";
+
 export const playlistSchema = joi.object<CreatePlaylistData>({
   title: joi.string().required(),
   icon: joi.string(),
