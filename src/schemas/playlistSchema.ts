d4208ae7540a835d64937a4a77d5326269f2a9e7
rafaela <rafaela.larissalj@gmail.com>
2022-06-20 18:59:40 -0300

    updating branch

--- src/schemas/playlistSchema.ts
+++ src/schemas/playlistSchema.ts
@@ -3,7 +3,7 @@ import { CreatePlaylistData } from "../services/playlistService.js";
 
 export const playlistSchema = joi.object<CreatePlaylistData>({
   title: joi.string().required(),
-  icon: joi.string().optional(),
-  description: joi.string().optional(),
+  icon: joi.string(),
+  description: joi.string(),
   createdAt: joi.date(),
 });
